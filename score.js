2d412f7f3e74bf219e8f36339e2b4d705d5e8212
Kyle White <kwdwhite@yahoo.com>
2018-04-11 09:33:44 -0400

    final

--- score.js
+++ score.js
@@ -12,22 +12,28 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     var body;
-      if(req.url !== "/scores"){
+      if(req.method === "GET") {
+        if(req.url !== "/scores"){
         res.statusCode = 404;
       } else {
-        if(req.method === "GET") {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/javascript');      
-          body = JSON.stringify(scores);
-      } else {
-        if(req.method === "POST") {
-          // req.body;
-          // console.log(req.body);
+          scores.sort((a, b)) => (b.score - a.score);
+          topScores = scores.slice(0,3);
+          body = JSON.stringify(topScores);
       }
-    }
-  }
-    res.end(body);  
-  });
+     } if (req.method === "POST") {
+          res.statusCode = 201;
+          jsonBody (req, res, (err, body) => {
+          scores.push(body);
+          })   
+        }
+       
+  //   res.end(body);
+  //   console.log(scores);
+  //   console.log(req.url);
+  //   console.log(req.method); 
+  // });
 
 
 server.listen(port, hostname, () => {
